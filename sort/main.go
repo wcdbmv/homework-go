7aa0da001b5ba9429c7760f78e70205218350fc7
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-09-14 03:59:46 +0300

    Separate main and sort, correct range loops, rename slices

---
+++ sort/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+var (
+	flags  Flags
+	output string
+)
+
+func init() {
+	flag.BoolVar(&flags.caseInsensitive, "f", false, "fold lower case to upper case characters")
+	flag.BoolVar(&flags.unique,          "u", false, "output only the first of an equal run")
+	flag.BoolVar(&flags.reverse,         "r", false, "reverse the result of comparisons")
+	flag.BoolVar(&flags.numeric,         "n", false, "compare according to string numerical value")
+	flag.IntVar(&flags.column,           "k", 0,     "sort by k column")
+	flag.StringVar(&output      ,        "o", "",    "write result to FILE instead of standard output")
+}
+
+func main() {
+	flag.Parse()
+
+	handle(checkArgs())
+
+	lines, err := ReadLines(flag.Args()[0])
+	handle(err)
+
+	sortedLines, err := Sorted(lines, flags)
+	handle(err)
+
+	handle(WriteLines(output, sortedLines))
+}
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func checkArgs() error {
+	if len(flag.Args()) != 1 {
+		flag.Usage()
+		return errors.New("flag.Args() != 1")
+	}
+	if flags.column < 0 {
+		return errors.New("column cannot be non-positive")
+	}
+	return nil
+}
+
+func ReadLines(filename string) ([]string, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	text := strings.Split(string(content), "\n")
+	if text[len(text) - 1] == "" {
+		text = text[:len(text) - 1]
+	}
+	return text, nil
+}
+
+func WriteLines(output string, text []string) error {
+	lines := strings.Join(text, "\n") + "\n"
+	if output != "" {
+		return ioutil.WriteFile(output, []byte(lines), 0644)
+	}
+	_, err := fmt.Print(lines)
+	return err
+}
--- sort/sort.go
+++ sort/sort.go
@@ -2,30 +2,18 @@ package main
 
 import (
 	"errors"
-	"flag"
 	"fmt"
-	"io/ioutil"
 	"sort"
 	"strconv"
 	"strings"
 )
 
-var (
+type Flags struct {
 	caseInsensitive bool
 	unique          bool
 	reverse         bool
 	numeric         bool
 	column          int
-	output          string
-)
-
-func init() {
-	flag.BoolVar(&caseInsensitive, "f", false, "fold lower case to upper case characters")
-	flag.BoolVar(&unique,          "u", false, "output only the first of an equal run")
-	flag.BoolVar(&reverse,         "r", false, "reverse the result of comparisons")
-	flag.BoolVar(&numeric,         "n", false, "compare according to string numerical value")
-	flag.IntVar(&column,           "k", 0,     "sort by k column")
-	flag.StringVar(&output,        "o", "",    "write result to FILE instead of standard output")
 }
 
 type StringMap struct {
@@ -33,127 +21,97 @@ type StringMap struct {
 	value *string
 }
 
-func main() {
-	flag.Parse()
+func Sorted(lines []string, flags Flags) ([]string, error) {
+	mappedLines := link(lines)
 
-	handle(checkArgs())
-
-	text, err := ReadLines(flag.Args()[0])
-	handle(err)
-
-	lines := link(text)
-
-	if column > 0 {
-		handle(extractColumn(lines, column))
-	}
-
-	if caseInsensitive {
-		lowerize(lines)
-	}
-
-	if unique {
-		if numeric {
-			lines, err = removeDuplicatesNumeric(lines)
-			handle(err)
-		} else {
-			lines = removeDuplicates(lines)
+	if flags.column > 0 {
+		if err := extractColumn(mappedLines, flags.column); err != nil {
+			return nil, err
 		}
 	}
 
-	less := func(i, j int) bool { return lines[i].key < lines[j].key }
+	if flags.caseInsensitive {
+		lowerize(mappedLines)
+	}
+
+	if flags.unique {
+		uniq, err := uniqueize(mappedLines, flags.numeric)
+		if err != nil {
+			return nil, err
+		}
+		mappedLines = uniq
+	}
+
+	less := func(i, j int) bool { return mappedLines[i].key < mappedLines[j].key }
+	if flags.numeric {
+		less = func(i, j int) bool { return atof(mappedLines[i].key) < atof(mappedLines[j].key) }
+	}
+	sort.Slice(mappedLines, less)
+
+	if flags.reverse {
+		for i, j := 0, len(mappedLines) - 1; i < j; i, j = i + 1, j - 1 {
+			mappedLines[i], mappedLines[j] = mappedLines[j], mappedLines[i]
+		}
+	}
+
+	return load(mappedLines), nil
+}
+
+func link(lines []string) []StringMap {
+	mappedLines := make([]StringMap, len(lines))
+	for i := range lines {
+		mappedLines[i] = StringMap{lines[i], &lines[i]}
+	}
+	return mappedLines
+}
+
+func extractColumn(mappedLines []StringMap, k int) error {
+	for i := range mappedLines {
+		words := strings.Fields(mappedLines[i].key)
+		if len(words) < k {
+			return errors.New(fmt.Sprint("Not enough columns in row %i", i))
+		}
+		mappedLines[i].key = words[k - 1]
+	}
+	return nil
+}
+
+func lowerize(mappedLines []StringMap) {
+	for i := range mappedLines {
+		mappedLines[i].key = strings.ToLower(mappedLines[i].key)
+	}
+}
+
+func uniqueize(lines []StringMap, numeric bool) ([]StringMap, error) {
+	removeDuplicatesFunc := removeDuplicates
 	if numeric {
-		less = func(i, j int) bool { return atof(lines[i].key) < atof(lines[j].key) }
+		removeDuplicatesFunc = removeDuplicatesNumeric
 	}
 
-	sort.Slice(lines, less)
-
-	if reverse {
-		for i, j := 0, len(lines) - 1; i < j; i, j = i + 1, j - 1 {
-			lines[i], lines[j] = lines[j], lines[i]
-		}
-	}
-
-	err = Write(output, join(lines))
-	handle(err)
+	return removeDuplicatesFunc(lines)
 }
 
-func checkArgs() error {
-	if len(flag.Args()) != 1 {
-		flag.Usage()
-		return errors.New("flag.Args() != 1")
-	}
-	if column < 0 {
-		return errors.New("column cannot be non-positive")
-	}
-	return nil
-}
-
-func ReadLines(filename string) ([]string, error) {
-	content, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	text := strings.Split(string(content), "\n")
-	if text[len(text) - 1] == "" {
-		text = text[:len(text) - 1]
-	}
-	return text, nil
-}
-
-func Write(output string, data string) error {
-	if output != "" {
-		return ioutil.WriteFile(output, []byte(data), 0644)
-	}
-	_, err := fmt.Print(data)
-	return err
-}
-
-func link(text []string) []StringMap {
-	lines := make([]StringMap, len(text))
-	for i, line := range text {
-		lines[i] = StringMap{line, &text[i]}
-	}
-	return lines
-}
-
-func extractColumn(lines []StringMap, k int) error {
-	for _, line := range lines {
-		strSlice := strings.Fields(line.key)
-		if len(strSlice) < k {
-			return errors.New(fmt.Sprint("Not enough column in row %i", k))
-		}
-		line.key = strSlice[k - 1]
-	}
-	return nil
-}
-
-func lowerize(lines []StringMap) {
-	for i, line := range lines {
-		lines[i].key = strings.ToLower(line.key)
-	}
-}
-
-func removeDuplicates(lines []StringMap) []StringMap {
+func removeDuplicates(mappedLines []StringMap) ([]StringMap, error) {
 	used := make(map[string]bool)
 	var unique []StringMap
-	for _, line := range lines {
+	for _, line := range mappedLines {
 		if _, value := used[line.key]; !value {
 			used[line.key] = true
 			unique = append(unique, line)
 		}
 	}
-	return unique
+	return unique, nil
 }
 
-func removeDuplicatesNumeric(lines []StringMap) ([]StringMap, error) {
+func removeDuplicatesNumeric(mappedLines []StringMap) ([]StringMap, error) {
 	used := make(map[float64]bool)
 	var unique []StringMap
-	for _, line := range lines {
+	for _, line := range mappedLines {
 		f, err := strconv.ParseFloat(line.key, 64)
 		if err != nil {
 			return nil, err
 		}
-		if _, value := used[f]; !value {
+		if _, was := used[f]; !was {
 			used[f] = true
 			unique = append(unique, line)
 		}
@@ -161,24 +119,15 @@ func removeDuplicatesNumeric(lines []StringMap) ([]StringMap, error) {
 	return unique, nil
 }
 
-func join(lines []StringMap) string {
-	if len(lines) == 0 {
-		return "\n"
+func load(mappedLines []StringMap) []string {
+	lines := make([]string, len(mappedLines))
+	for i := range mappedLines {
+		lines[i] = *mappedLines[i].value
 	}
-	var text string
-	for _, line := range lines {
-		text = text + *line.value + "\n"
-	}
-	return text
+	return lines
 }
 
 func atof(str string) float64 {
 	f, _ := strconv.ParseFloat(str, 64)
 	return f
-}
-
-func handle(err error) {
-	if err != nil {
-		panic(err)
-	}
 }
